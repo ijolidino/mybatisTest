0bd3c60dfedf66efb66cf01db9b3f7da0016ecaa
ijolidino <ijunido@gmail.com>
2022-09-13 22:41:04 +0800

    添加单元测试

---
+++ src/test/java/com/blackmagicwoman/mybatistest/SpringbootTest.java
@@ -0,0 +1,36 @@
+package com.blackmagicwoman.mybatistest;
+
+import com.blackmagicwoman.mybatistest.entity.PmsCategory;
+import com.blackmagicwoman.mybatistest.mapper.PmsCategoryMapper;
+import javafx.application.Application;
+import org.junit.Test;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * @program: mybatisTest
+ * @description: 项目测试
+ * @author: Fuwen
+ * @create: 2022-09-13 22:19
+ **/
+@SpringBootTest(classes = MybatisTestApplication.class)
+@RunWith(SpringRunner.class)
+public class SpringbootTest {
+
+    //private static final Logger logger = (Logger) LoggerFactory.getLogger(SpringbootTest.class);
+
+    @Autowired
+    private PmsCategoryMapper pmsCategoryMapper;
+
+    @Test
+    public void testApi(){
+        List<PmsCategory> query = pmsCategoryMapper.query(new PmsCategory());
+        System.out.println(query.toString());
+    }
+}
