8c9996ebf68f82683f5d9cbbc97625cf85618793
ijolidino <ijunido@gmail.com>
2022-09-16 21:42:13 +0800

    练习flatmap操作

--- src/test/java/com/blackmagicwoman/mybatistest/FuntionalTest.java
+++ src/test/java/com/blackmagicwoman/mybatistest/FuntionalTest.java
@@ -5,6 +5,7 @@ import com.blackmagicwoman.mybatistest.service.funtion.HandlerStringService;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @program: mybatisTest
@@ -25,5 +26,28 @@ public class FuntionalTest {
         List<String> strings = Arrays.asList("1", "2", "3", "4", "5");
         strings.sort((o1, o2) -> o2.compareTo(o1));//[5, 4, 3, 2, 1]
         System.out.println(strings);
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(5, 4);
+        List<Integer> numbers3 = Arrays.asList(10,11,12,13);
+        List<int[]> pairs =
+                numbers1.stream()
+                        .flatMap(i -> numbers2.stream()
+                                .map(j -> new int[]{i, j})
+                        )
+                        .collect(Collectors.toList());
+        System.out.println(pairs);
+        List<Integer> collect = numbers2.stream().flatMap(i -> numbers1.stream()
+                .filter(j -> i % j == 0)).collect(Collectors.toList());
+        System.out.println(collect);//[1, 1, 2]
+        List<List<Integer>> collect1 = numbers1.stream()
+                .flatMap(i ->
+                        numbers2.stream()
+                                .filter(j -> (i + j) % 3 == 0)
+                                .map(j -> Arrays.asList(i, j))
+                )
+                .collect(Collectors.toList());
+        System.out.println(collect1);//[[1, 5], [2, 4]]
+        List<Integer> collect2 = numbers2.stream().flatMap(i -> numbers3.stream().filter(j -> j * i > 55)).distinct().collect(Collectors.toList());
+        System.out.println(collect2);
     }
 }
