9a7094d8691976df5682f18309d61d508a7ef2ce
ijolidino <ijunido@gmail.com>
2023-06-23 11:46:42 +0800

    这是第一次提交的gitLog的信息

---
+++ src/main/java/com/blackmagicwoman/test/git/commit/TestGit.java
@@ -0,0 +1,17 @@
+package com.blackmagicwoman.test.git.commit;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @program: mybatisTest
+ * @description: 测试git练习的提交
+ * @author: Fuwen
+ * @create: 2023-06-23 11:44
+ **/
+@Slf4j
+public class TestGit {
+
+    public static void main(String[] args) {
+        log.info("这是第一次提交的内容");
+    }
+}
