0923d7e194268090fa57c273af24f99bc29e2d4f
ijolidino <ijunido@gmail.com>
2023-08-05 00:32:35 +0800

    测试学

---
+++ src/main/java/com/blackmagicwoman/mybatistest/helloworld/controller/SpringBootTestForLearnEnglish.java
@@ -0,0 +1,19 @@
+package com.blackmagicwoman.mybatistest.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: mybatisTest
+ * @description:
+ * @author: Fuwen
+ * @create: 2022-12-14 20:51
+ **/
+@RestController
+public class SpringBootTestForLearnEnglish {
+
+    @RequestMapping
+    public String helloWorld(){
+        return "Hello World from Spring Boot";
+    }
+}
