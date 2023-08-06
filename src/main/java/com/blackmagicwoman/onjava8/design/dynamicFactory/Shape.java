68bdff1c59cc040c669d741df46b1dd7ccae2e55
ijolidino <ijunido@gmail.com>
2023-08-06 21:19:12 +0800

    形状类测试

---
+++ src/main/java/com/blackmagicwoman/onjava8/design/dynamicFactory/Shape.java
@@ -0,0 +1,22 @@
+package com.blackmagicwoman.onjava8.design.dynamicFactory;
+
+/**
+ * @program: mybatisTest
+ * @description: 形状
+ * @author: Fuwen
+ * @create: 2022-11-13 09:58
+ **/
+public class Shape {
+    private static int counter = 0;
+    private int id = counter++;
+    @Override public String toString() {
+        return
+                getClass().getSimpleName() + "[" + id + "]";
+    }
+    public void draw() {
+        System.out.println(this + " draw");
+    }
+    public void erase() {
+        System.out.println(this + " erase");
+    }
+}
