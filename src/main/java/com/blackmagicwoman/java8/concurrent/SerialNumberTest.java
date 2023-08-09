3941e90ccf755aad22ecff742d7205f42d61490b
ijolidino <ijunido@gmail.com>
2023-08-09 22:14:36 +0800

    序列号生成器测试

---
+++ src/main/java/com/blackmagicwoman/java8/concurrent/SerialNumberTest.java
@@ -0,0 +1,13 @@
+package com.blackmagicwoman.java8.concurrent;
+
+/**
+ * @program: mybatisTest
+ * @description: 序列号测试
+ * @author: Fuwen
+ * @create: 2023-08-08 22:10
+ **/
+public class SerialNumberTest {
+    public static void main(String[] args) {
+        SerialNumberChecker.test(new SerialNumbers());
+    }
+}
