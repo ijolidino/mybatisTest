997c2e24ab070a396ba0a7c881f3a6959a6a5e05
ijolidino <ijunido@gmail.com>
2023-08-12 17:05:38 +0800

    从内存中输入人

---
+++ src/main/java/com/blackmagicwoman/file/test/MemoryInput.java
@@ -0,0 +1,18 @@
+package com.blackmagicwoman.file.test;
+
+import org.apache.coyote.http11.filters.BufferedInputFilter;
+
+import java.io.StringReader;
+
+/**
+ * @program: mybatisTest
+ * @description: 从内存中输入人
+ * @author: Fuwen
+ * @create: 2023-08-11 21:46
+ **/
+public class MemoryInput {
+
+    public static void main(String[] args) {
+
+    }
+}
