dddc55e1e122b75b591982cf9a49b93446dd49d2
ijolidino <ijunido@gmail.com>
2023-08-17 21:42:30 +0800

    工厂模式测试

---
+++ src/main/java/com/blackmagicwoman/onjava8/design/dynamicFactory/FactoryTest.java
@@ -0,0 +1,23 @@
+package com.blackmagicwoman.onjava8.design.dynamicFactory;
+
+import java.util.stream.Stream;
+
+/**
+ * @program: mybatisTest
+ * @description: 测试工厂
+ * @author: Fuwen
+ * @create: 2022-11-13 09:55
+ **/
+public class FactoryTest {
+    /**
+     *
+     */
+    public static void test(FactoryMethod factory) {
+//        Stream.of("Circle", "Square", "Triangle",
+//                        "Square", "Circle", "Circle", "Triangle")
+//                .map(factory::create)
+//                .peek(Shape::draw)
+//                .peek(Shape::erase)
+//                .count(); // Terminal operation
+    }
+}
