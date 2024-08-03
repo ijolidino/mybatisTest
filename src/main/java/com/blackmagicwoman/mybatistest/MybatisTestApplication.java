6f77440409090a2c01b95dcc8830a475f6046de0
ijolidino <ijunido@gmail.com>
2024-08-03 16:50:58 +0800

    调整主启动类的配置

--- src/main/java/com/blackmagicwoman/mybatistest/MybatisTestApplication.java
+++ src/main/java/com/blackmagicwoman/mybatistest/MybatisTestApplication.java
@@ -1,11 +1,17 @@
 package com.blackmagicwoman.mybatistest;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.blackmagicwoman"})
 @EnableAspectJAutoProxy
+@MapperScan({"com.blackmagicwoman.mybatistest.mapper"})
+@EnableTransactionManagement
 public class MybatisTestApplication {
 
     public static void main(String[] args) {
