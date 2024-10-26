0b7d792131fe210cb7b3e757ab28484f2c7bf58d
ijolidino <ijunido@gmail.com>
2024-10-26 13:23:13 +0800

    添加正则表达式的标签

---
+++ src/main/java/com/blackmagicwoman/docTplEexcutor/constant/ExpType.java
@@ -0,0 +1,8 @@
+package com.blackmagicwoman.docTplEexcutor.constant;
+
+public class ExpType {
+    /**列表标签循环表达式**/
+    public static final String EXP_TYPE_1="1";
+    /**逻辑判断表达式**/
+    public static final String EXP_TYPE_2="2";
+}
